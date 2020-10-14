5f73ceb0217e79b11fedb75ab8b789c0fd4f88d0
katr6421 <katr6421@stud.kea.dk>
2020-10-14 16:35:33 +0200

    sdfdsf

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,34 @@ const godEnergiContainer = document.querySelector("#energi_container");
 const godEnergiContainerDiver = document.querySelector("#energi_container > div");
 const diverIOverskud = document.querySelectorAll("#overskudIHverdagen > div");
 const vidsteDuAtSection = document.querySelector("#vidsteDuAtContainer");
+const DemDerSkalPoppeUpEnergi = document.querySelector(".popup");
+const demDerSkalPoppeUpHverdagspauser = document.querySelector = ("#hverdagensPauser > div");
+
+const demDerSkalPoppeUpGodsamvitighed = document.querySelector = ("#godSamvittighed > div");
+const demDerSkalPoppeUpVidste = document.querySelector = ("#vidsteDuAt > div");
+
+//const faktaboks1 = document.querySelector = ("fakta1");
+//const faktaboks2 = document.querySelector = (".fakta2");
+//const faktaboks3 = document.querySelector = (".fakta3");
+//const faktaboks4 = document.querySelector = (".fakta4");
+//
+//window.addEventListener("load", visStart);
+//
+//function visStart() {
+//    faktaboks1.addEventListener("click", clickFakta1);
+//}
+//
+//
+//
+//function clickFakta1() {
+//    faktaboks1.classList.remove(".pulse");
+//    faktaboks1.classList.add(".scale")
+//    console.log("jeg har klikket på fakaboks1")
+//}
+
+
+
+
 
 
 gsap.set("#energi_container", {
@@ -60,6 +88,62 @@ gsap.from(diverIOverskud, {
     stagger: 1,
     scrollTrigger: {
         trigger: "#overskudIHverdagen",
+        start: "10% center",
+        end: "center center",
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from(DemDerSkalPoppeUpEnergi, {
+    opacity: 0,
+    y: 100,
+    duration: 2,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#start1",
+        start: "10% center",
+        end: "center center",
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from(demDerSkalPoppeUpHverdagspauser, {
+    opacity: 0,
+    y: 100,
+    duration: 2,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#hverdagensPauser",
+        start: "10% center",
+        end: "center center",
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from(demDerSkalPoppeUpGodsamvitighed, {
+    opacity: 0,
+    y: 100,
+    duration: 2,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#godSamvittighed",
+        start: "10% center",
+        end: "center center",
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from(demDerSkalPoppeUpVidste, {
+    opacity: 0,
+    y: 100,
+    duration: 2,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#vidsteDuAt",
         start: "10% center",
         end: "center center",
         markers: true,
